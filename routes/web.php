9670bc09e08cce254524d0f99dd3e842b0c588ec
o-Bunny-o <pjudithoh@gmail.com>
2024-11-04 13:03:30 -0500

    routes beginning

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\SearchController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// home page / book list
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+
+// R+book
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+
+// form submission
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+
+
