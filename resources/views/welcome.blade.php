448154f273d192ef745cf5b5dfbc2caef9036397
o-Bunny-o <pjudithoh@gmail.com>
2024-11-04 12:59:24 -0500

    little corrections

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <title>Bienvenue</title>
 </head>
 <body>
-    <h1>Bienvenue dans la Bibliothèque</h1>
+    <h1>Bienvenue Chez La Fleur des Livres</h1>
     <p>il fault mettre du texte ici :P</p>
     <a href="{{ route('books.index') }}">Voir la Liste des Livres</a>
 </body>
